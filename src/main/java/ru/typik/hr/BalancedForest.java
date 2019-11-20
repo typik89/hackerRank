5292b62bab9f0c14938813dc21bc30135deb2eb4
typik <typik89@yandex.ru>
2019-11-20 19:41:59 +0300

    balancedForest time limits didn't passed

--- src/main/java/ru/typik/hr/BalancedForest.java
+++ src/main/java/ru/typik/hr/BalancedForest.java
@@ -4,30 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BalancedForest {
-	
-	public static class Node{
+
+	public static class Node {
 		private int id;
 		private int value;
 		private List<Node> connections = new ArrayList<>();
-		public Node(int id,int value) {
+
+		public Node(int id, int value) {
 			this.value = value;
 			this.setId(id);
 		}
+
 		public int getValue() {
 			return value;
 		}
+
 		public void removeConnection(Node node) {
-			connections.remove( node );
+			connections.remove(node);
 		}
+
 		public void addConnection(Node node) {
-			connections.add( node );
+			connections.add(node);
 		}
+
 		public List<Node> getConnections() {
 			return connections;
 		}
+
 		public int getId() {
 			return id;
 		}
+
 		public void setId(int id) {
 			this.id = id;
 		}
@@ -36,83 +43,92 @@ public class BalancedForest {
 	public static int balancedForest(int[] values, int[][] edges) {
 		Node[] nodes = new Node[values.length];
 		Integer minResult = null;
-		for( int i = 0; i < values.length; ++i ) {
+		for (int i = 0; i < values.length; ++i) {
 			nodes[i] = new Node(i, values[i]);
 		}
-		for( int[] edge : edges ) {
+		for (int[] edge : edges) {
 			add(nodes, edge);
 		}
-		for( int i = 0; i < edges.length - 1; ++i ) {
+		for (int i = 0; i < edges.length - 1; ++i) {
 			cut(nodes, edges[i]);
-			for( int j = 0; j < edges.length; ++j ) {
+			for (int j = i + 1; j < edges.length; ++j) {
 				cut(nodes, edges[j]);
 				int[] graphIndex = new int[nodes.length];
 				int graphNum = 0;
-				for( Node node : nodes ) {
-					if ( graphIndex[node.getId()] < 1 ) {
+				for (Node node : nodes) {
+					if (graphIndex[node.getId()] < 1) {
 						graphNum++;
-						walkToGraphAndMark(node,graphIndex,graphNum);
+						walkToGraphAndMark(node, graphIndex, graphNum);
 					}
-					if ( graphNum == 3 ) {
+					if (graphNum == 3) {
 						break;
 					}
 				}
-				long sum1 = getSum(nodes,graphIndex,1);
-				long sum2 = getSum(nodes,graphIndex,2);
-				long sum3 = getSum(nodes,graphIndex,3);
-				int newResult = getValueToAdd( sum1 , sum2 , sum3 );
-				minResult = minResult == null || newResult < minResult ? 
-						newResult : minResult;
-				add(nodes,edges[j]);
+				// System.out.println("graphIndex: " +
+				// IntStream.of(graphIndex).boxed().collect(Collectors.toList()));
+				long sum1 = getSum(nodes, graphIndex, 1);
+				long sum2 = getSum(nodes, graphIndex, 2);
+				long sum3 = getSum(nodes, graphIndex, 3);
+				// System.out.println(String.format("Sums : %s , %s , %s", sum1, sum2, sum3));
+				Integer newResult = getValueToAdd(sum1, sum2, sum3);
+				if (newResult != null) {
+					minResult = minResult == null || newResult < minResult ? newResult : minResult;
+				}
+				add(nodes, edges[j]);
 			}
-			add(nodes,edges[i]);
+			add(nodes, edges[i]);
 		}
 		return minResult == null ? -1 : minResult;
 	}
-	
-	private static int getValueToAdd(long sum1, long sum2, long sum3) {
-		
+
+	private static Integer getValueToAdd(long sum1, long sum2, long sum3) {
+		if (sum1 == sum2 && sum3 < sum1) {
+			return (int) (sum1 - sum3);
+		} else if (sum2 == sum3 && sum1 < sum2) {
+			return (int) (sum2 - sum1);
+		} else if (sum1 == sum3 && sum2 < sum1) {
+			return (int) (sum1 - sum2);
+		} else if (sum1 + sum2 == sum3) {
+			return (int) sum3;
+		} else if (sum2 + sum3 == sum1) {
+			return (int) sum1;
+		} else if (sum1 + sum3 == sum2) {
+			return (int) sum2;
+		} else {
+			return null;
+		}
 	}
 
 	private static long getSum(Node[] nodes, int[] graphIndex, int graphNum) {
 		long sum = 0;
-		for( int i = 0; i < graphIndex.length; ++i) {
-			if ( graphIndex[i] == graphNum ) {
+		for (int i = 0; i < graphIndex.length; ++i) {
+			if (graphIndex[i] == graphNum) {
 				sum += nodes[i].getValue();
 			}
 		}
 		return sum;
-		
+
 	}
 
 	private static void walkToGraphAndMark(Node node, int[] graphIndex, int graphNum) {
 		graphIndex[node.getId()] = graphNum;
-		for( Node connection : node.getConnections() ) {
-			if ( graphIndex[node.getId()] < 1 ) {
+		for (Node connection : node.getConnections()) {
+			if (graphIndex[connection.getId()] < 1) {
 				walkToGraphAndMark(connection, graphIndex, graphNum);
 			}
 		}
 	}
 
-	private static void cut(Node[] nodes,int[] edge) {
-		nodes[edge[1]-1].removeConnection(nodes[edge[0]-1]);
-		nodes[edge[0]-1].removeConnection(nodes[edge[1]-1]);
+	private static void cut(Node[] nodes, int[] edge) {
+		// System.out.println(String.format("Cutting %s -> %s", edge[0], edge[1]));
+		nodes[edge[1] - 1].removeConnection(nodes[edge[0] - 1]);
+		nodes[edge[0] - 1].removeConnection(nodes[edge[1] - 1]);
 	}
-	private static void add( Node[] nodes , int[] edge ) {
-		nodes[edge[0]-1].addConnection( nodes[edge[1]-1] );
-		nodes[edge[1]-1].addConnection( nodes[edge[0]-1] );		
+
+	private static void add(Node[] nodes, int[] edge) {
+		// System.out.println(String.format("Adding %s -> %s", edge[0], edge[1]));
+		nodes[edge[0] - 1].addConnection(nodes[edge[1] - 1]);
+		nodes[edge[1] - 1].addConnection(nodes[edge[0] - 1]);
 	}
-	
-	private static long getSum(Node node,boolean[] passed) {
-		long sum = node.getValue();
-		passed[node.getId()] = true;
-		for( Node connection : node.getConnections() ) {
-			if ( !passed[connection.getId()] ) {
-				sum += getSum(connection, passed);
-			}
-		}
-		return sum;
-	}
-	
-	
+
 }
--- src/test/java/ru/typik/hr/BalancedForestTest.java
+++ src/test/java/ru/typik/hr/BalancedForestTest.java
@@ -12,103 +12,78 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class BalancedForestTest {
-	
+
 	@Test
 	public void test1() {
-		assertEquals( 19 , 
-				BalancedForest.balancedForest(
-						new int[] { 15, 12, 8, 14, 13 },
-						new int[][] { 
-							new int[] { 1,2 },
-							new int[] { 1,3 },
-							new int[] { 1,4 },
-							new int[] { 4,5 }
-						}) );	
-		if ( true ) {
-			return;
-		}
-		
-		
-		assertEquals( 2 , 
-				BalancedForest.balancedForest(
-					new int[] { 1, 2, 2, 1, 1, },
-					new int[][] {
-						new int[] { 1, 2 },
-						new int[] { 1, 3 },
-						new int[] { 3, 5 },
-						new int[] { 1, 4 } } ) );		
-		assertEquals( -1 , 
-				BalancedForest.balancedForest(
-					new int[] { 1, 3, 5 },
-					new int[][] {
-						new int[] { 1, 3 },
-						new int[] { 1, 2 }
-					}) );			
-		assertEquals( 4 , 
-				BalancedForest.balancedForest(
-					new int[] { 12, 10, 8, 12, 14, 12 },
-					new int[][] {
-						new int[] { 1, 2 },
-						new int[] { 1, 3 },
-						new int[] { 1, 4 },
-						new int[] { 2, 5 },
-						new int[] { 4, 6 } } ) );
+		assertEquals(19, BalancedForest.balancedForest(new int[] { 15, 12, 8, 14, 13 },
+				new int[][] { new int[] { 1, 2 }, new int[] { 1, 3 }, new int[] { 1, 4 }, new int[] { 4, 5 } }));
+
+		assertEquals(2, BalancedForest.balancedForest(new int[] { 1, 2, 2, 1, 1, },
+				new int[][] { new int[] { 1, 2 }, new int[] { 1, 3 }, new int[] { 3, 5 }, new int[] { 1, 4 } }));
+		assertEquals(-1, BalancedForest.balancedForest(new int[] { 1, 3, 5 },
+				new int[][] { new int[] { 1, 3 }, new int[] { 1, 2 } }));
+		assertEquals(4, BalancedForest.balancedForest(new int[] { 12, 10, 8, 12, 14, 12 }, new int[][] {
+				new int[] { 1, 2 }, new int[] { 1, 3 }, new int[] { 1, 4 }, new int[] { 2, 5 }, new int[] { 4, 6 } }));
 	}
-	
-	
-	
+
 	@Test
 	public void testFile() throws IOException {
-		List<Parameters> parametersList = getParametersFromFile( "src/test/resources/balancedTree/input01.txt" );
-		assertEquals( 5 , parametersList.size() );
-		
-		//assertEquals( -1  , BalancedForest.balancedForest( parametersList.get( 0 ).getValues(), parametersList.get( 0 ).getEdges() ) );
-		assertEquals( 10  , BalancedForest.balancedForest( parametersList.get( 1 ).getValues(), parametersList.get( 1 ).getEdges() ) );
-		//assertEquals( 13  , BalancedForest.balancedForest( parametersList.get( 0 ).getValues(), parametersList.get( 0 ).getEdges() ) );
-		//assertEquals( 5   , BalancedForest.balancedForest( parametersList.get( 0 ).getValues(), parametersList.get( 0 ).getEdges() ) );
-		//assertEquals( 297 , BalancedForest.balancedForest( parametersList.get( 0 ).getValues(), parametersList.get( 0 ).getEdges() ) );
+		List<Parameters> parametersList = getParametersFromFile("src/test/resources/balancedTree/input01.txt");
+		assertEquals(5, parametersList.size());
+
+		assertEquals(-1,
+				BalancedForest.balancedForest(parametersList.get(0).getValues(), parametersList.get(0).getEdges()));
+		assertEquals(10,
+				BalancedForest.balancedForest(parametersList.get(1).getValues(), parametersList.get(1).getEdges()));
+		assertEquals(13,
+				BalancedForest.balancedForest(parametersList.get(2).getValues(), parametersList.get(2).getEdges()));
+		assertEquals(5,
+				BalancedForest.balancedForest(parametersList.get(3).getValues(), parametersList.get(3).getEdges()));
+		assertEquals(297,
+				BalancedForest.balancedForest(parametersList.get(4).getValues(), parametersList.get(4).getEdges()));
 	}
 
-
-
 	private List<Parameters> getParametersFromFile(String path) throws IOException {
-		List<String> lines = Files.readAllLines( Paths.get( path ) );
+		List<String> lines = Files.readAllLines(Paths.get(path));
 		List<Parameters> parametersList = new ArrayList<>();
 		int i = 1;
-		while( i < lines.size() ) {
-			int count = Integer.parseInt( lines.get(i) );
+		while (i < lines.size()) {
+			int count = Integer.parseInt(lines.get(i));
 			Parameters parameters = new Parameters();
-			parameters.setValues( Stream.of( lines.get( i + 1 ).split( " " ) ).mapToInt( el -> Integer.parseInt( el ) ).toArray() );
-			parameters.setEdges( new int[count-1][] );
-			for( int j = 0; j < count - 1; ++j ) {
-				parameters.getEdges()[j] = getIntArrayFromString( lines.get( i + 2 + j ) );
+			parameters.setValues(Stream.of(lines.get(i + 1).split(" ")).mapToInt(el -> Integer.parseInt(el)).toArray());
+			parameters.setEdges(new int[count - 1][]);
+			for (int j = 0; j < count - 1; ++j) {
+				parameters.getEdges()[j] = getIntArrayFromString(lines.get(i + 2 + j));
 			}
 			parametersList.add(parameters);
 			i += count + 1;
 		}
 		return parametersList;
 	}
-	
-	private static int[] getIntArrayFromString( String valueString ) {
-		return Stream.of( valueString.split( " " ) ).mapToInt( el -> Integer.parseInt( el ) ).toArray();
+
+	private static int[] getIntArrayFromString(String valueString) {
+		return Stream.of(valueString.split(" ")).mapToInt(el -> Integer.parseInt(el)).toArray();
 	}
-	
-	public static class Parameters{
+
+	public static class Parameters {
 		private int[] values;
 		private int[][] edges;
+
 		public int[][] getEdges() {
 			return edges;
 		}
+
 		public void setEdges(int[][] edges) {
 			this.edges = edges;
 		}
+
 		public int[] getValues() {
 			return values;
 		}
+
 		public void setValues(int[] values) {
 			this.values = values;
 		}
 	}
-
 
 }
